077bad9fa4b7e9c3a3c35d2442b919637914ad34
RJ Ocampo <rjqocampo@gmail.com>
2023-11-01 22:24:56 +0800

    Initial commit. Make script.js and setup ESLint

---
+++ script.js
@@ -0,0 +1,12 @@
+/* 
+Build a function mergeSort that takes in an array and returns a sorted array, using a recursive merge sort methodology.
+Tips:
+Think about what the base case is and what behavior is happening again and again and can actually be delegated to someone else (e.g. that same function!).
+It may be helpful to check out the background videos again if you don’t quite understand what should be going on.
+*/
+
+function merge() {
+  console.log("hello world");
+}
+
+merge();
